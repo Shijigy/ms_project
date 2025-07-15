a1c3556a2deb5632ab7582de594ca3fe22b29c0c
Shijigy <166893113+Shijigy@users.noreply.github.com>
2025-07-15 09:59:34 +0800

    登录安全性-绑定客户端ip

--- project-common/jwts/jwts.go
+++ project-common/jwts/jwts.go
@@ -14,20 +14,20 @@ type JwtToken struct {
 	RefreshExp   int64  `json:"refresh_exp"`
 }
 
-func CreateToken(val string, exp time.Duration, secret string, refreshExp time.Duration, refreshSecret string) *JwtToken {
+func CreateToken(val string, exp time.Duration, secret string, refreshExp time.Duration, refreshSecret string, ip string) *JwtToken {
 	aExp := time.Now().Add(exp).Unix()
 	accessToken := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
 		"token": val,
 		"exp":   aExp,
+		"ip":    ip,
 	})
 	aToken, _ := accessToken.SignedString([]byte(secret))
 	rExp := time.Now().Add(refreshExp).Unix()
 	refreshToken := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
 		"token": val,
-		"exp":   rExp,
+		"exp":   aExp,
 	})
 	rToken, _ := refreshToken.SignedString([]byte(refreshSecret))
-
 	return &JwtToken{
 		AccessExp:    aExp,
 		AccessToken:  aToken,
@@ -36,7 +36,7 @@ func CreateToken(val string, exp time.Duration, secret string, refreshExp time.D
 	}
 }
 
-func ParseToken(tokenString string, secret string) (string, error) {
+func ParseToken(tokenString string, secret string, ip string) (string, error) {
 	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
 		// Don't forget to validate the alg is what you expect:
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
@@ -54,6 +54,9 @@ func ParseToken(tokenString string, secret string) (string, error) {
 		exp := int64(claims["exp"].(float64))
 		if exp <= time.Now().Unix() {
 			return "", errors.New("token过期了")
+		}
+		if claims["ip"] != ip {
+			return "", errors.New("ip不合法")
 		}
 		return val, nil
 	} else {
--- project-user/pkg/service/login.service.v1/login_service.go
+++ project-user/pkg/service/login.service.v1/login_service.go
@@ -186,7 +186,8 @@ func (ls *LoginService) Login(ctx context.Context, msg *login.LoginMessage) (*lo
 	memIdStr := strconv.FormatInt(mem.Id, 10)
 	exp := time.Duration(config.C.JwtConfig.AccessExp*3600*24) * time.Second
 	rExp := time.Duration(config.C.JwtConfig.RefreshExp*3600*24) * time.Second
-	token := jwts.CreateToken(memIdStr, exp, config.C.JwtConfig.AccessSecret, rExp, config.C.JwtConfig.RefreshSecret)
+	token := jwts.CreateToken(memIdStr, exp, config.C.JwtConfig.AccessSecret, rExp, config.C.JwtConfig.RefreshSecret, msg.Ip)
+	// 可以给token做加密处理 增强安全性
 	tokenList := &login.TokenMessage{
 		AccessToken:    token.AccessToken,
 		RefreshToken:   token.RefreshToken,
@@ -212,7 +213,7 @@ func (ls *LoginService) TokenVerify(ctx context.Context, msg *login.LoginMessage
 	if strings.Contains(token, "bearer") {
 		token = strings.ReplaceAll(token, "bearer", "")
 	}
-	parseToken, err := jwts.ParseToken(token, config.C.JwtConfig.AccessSecret)
+	parseToken, err := jwts.ParseToken(token, config.C.JwtConfig.AccessSecret, msg.Ip)
 	if err != nil {
 		zap.L().Error("TokenVerify ParseToken error", zap.Error(err))
 		return nil, errs.GrpcError(model.NoLogin)
